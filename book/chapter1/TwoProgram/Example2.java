335f374552aa5c69a85b37236a033f5846048033
4FStorm <shevchuc@rambler.ru>
2020-04-03 21:57:49 +0300

    Update Example2.java
    судя по логике там должно быть имя переменной именно var2

--- book/chapter1/TwoProgram/Example2.java
+++ book/chapter1/TwoProgram/Example2.java
@@ -13,7 +13,7 @@ class Example2 {
 
 		var2 = var1 / 2;
 
-		System.out.print("Переменная var1 содержит var1 / 2: ");
+		System.out.print("Переменная var2 содержит var1 / 2: ");
 		System.out.println(var2);
 	}
 }
